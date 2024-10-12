1b0d435edf601a07b0cf959d9e5a6513f70b2798
happmaoo <happmaoo@gmail.com>
2024-10-13 06:13:48 +0800

    备份

--- git.sh
+++ git.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-date=date +"%Y-%m-%d %H:%M:%S"
-xfce4-terminal -e 'bash -c "git add .;git commit -m "备份";git push;bash"' -T "Run and ready"
+
+cmd2=$(cat <<EOF
+git add .;git commit -m "备份 $(date +'%Y-%m-%d %H:%M:%S')";git push;bash
+EOF
+)
+
+cmd=$(cat <<EOF
+bash -c "$cmd2"
+EOF
+)
+
+xfce4-terminal -e "$cmd" -T "Run and ready"
+
+#xfce4-terminal -e 'bash -c "git add .;git commit -m "备份";git push;bash"' -T "Run and ready"
