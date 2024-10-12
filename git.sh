1443fbc84a3149b1833821c68e77d78098728882
happmaoo <happmaoo@gmail.com>
2024-10-13 06:25:09 +0800

    备份 2024-10-13 06:25:09

--- git.sh
+++ git.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 
-xfce4-terminal -e 'bash -c "git add .;git commit -m "备份";git push;bash"' -T "Run and ready"
+#xfce4-terminal -e 'bash -c "git add .;git commit -m "备份";git push;bash"' -T "Run and ready"
+
+xfce4-terminal -e "bash -c 'git add .;git commit -m \"备份 $(date +'%Y-%m-%d %H:%M:%S')\";git push; bash'"
