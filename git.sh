ee15240d4c5317ad4d583b155e93db24e37e8a9c
happmaoo <happmaoo@gmail.com>
2025-12-24 15:45:30 +0800

    备份 2025-12-24 15:45:30

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #xfce4-terminal -e 'bash -c "git add .;git commit -m "备份";git push;bash"' -T "Run and ready"
-
+# 说明，先复制config文件到.git目录覆盖，然后再运行本脚本
 
 xfce4-terminal -e "bash -c 'git add .;printf \"%b\n\" \"$(git status --short)\";git commit -m \"备份 $(date +'%Y-%m-%d %H:%M:%S')\";git push;echo "完成.";sleep 5'"
 
