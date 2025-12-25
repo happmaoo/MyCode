f5faef88d810a79a350c33abae642457229b609d
happmaoo <happmaoo@gmail.com>
2025-12-25 10:51:12 +0800

    备

--- 备份/gattclient1.0/app/src/main/java/com/example/myapplication/GattConnectionStateListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication;
-
-public interface GattConnectionStateListener {
-    void onConnectionStateChanged(int newState);
-    void onCharacteristicRW(String value); // 新增方法
-}
