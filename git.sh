b4a7886cb04dc76032de78219314d5776a4d2c7e
happmaoo <happmaoo@gmail.com>
2025-12-25 10:57:13 +0800

    备

--- git.sh
+++ git.sh
@@ -5,9 +5,8 @@
 
 
 echo "清理 .gitignore 文件中的 /build 行"
-find . -name ".gitignore" -exec sed -i '' '/^\/build$/d' {} \;
+find . -name ".gitignore" -exec sed -i '/^\/build$/d' {} \;
 echo "完成"
-
 
 xfce4-terminal -e "bash -c 'git add .;printf \"%b\n\" \"$(git status --short)\";git commit -m \"备份 $(date +'%Y-%m-%d %H:%M:%S')\";git push;echo "完成.";sleep 5'"
 
